46a0eea9eef39f7ae5bb81b4ace51221bec3610c
christancf <it20610302@my.sliit.lk>
2023-05-19 17:07:16 +0530

    Table feature initiated

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,15 @@
+import { Paper } from '@mui/material'
 import React from 'react'
+import FFTable from '../../components/molecules/FFTable/FFTable'
 
 const index = () => {
   return (
     <>
       <h1 className="hidden">Home</h1>
+      {/* header */}
+      {/* <Paper className="shadow-sm rounded-xl xl:py-3 xl:px-20 mx-16 mt-2"></Paper> */}
+      {/* single row */}
+      <FFTable />
     </>
   )
 }
