a545f7e12973243451f2ab3e5b61c6dd7358d65d
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-20 10:13:12 +0530

    Deleted unused constatn file

--- src/components/atoms/FFRating/FFRating.jsx
+++ src/components/atoms/FFRating/FFRating.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Rating, Typography } from '@mui/material'
-import { RatingLables } from '../../../constants/RatingLables'
+
 const FFRating = ({ value, handleRatingChange }) => {
   return (
     <>
-      <Rating
-        name="half-rating"
-        defaultValue={0}
-        value={value}
-        precision={0.5}
-        onChange={handleRatingChange}
-      />
-      {RatingLables.value}
-      <Typography>{RatingLables.value}</Typography>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Rating
+          name="half-rating"
+          defaultValue={0}
+          value={value}
+          precision={0.5}
+          onChange={handleRatingChange}
+        />
+        <Typography>{value}</Typography>
+      </div>
     </>
   )
 }
