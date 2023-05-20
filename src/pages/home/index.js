98c4749ded1ba1aaebc2f6f9dc247572439efc95
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-20 10:12:51 +0530

    FFRating atom added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,20 +5,14 @@ import ColouredWarningIcon from '../../assets/icons/ColouredWarningIcon'
 import FFRating from '../../components/atoms/FFRating/FFRating'
 
 const Index = () => {
-  const [rating, setRating] = useState(0)
-  const handleRatingChange = (value) => {
-    setRating(value)
-  }
+  const [value, setValue] = useState(0)
   return (
     <>
       <h1 className="hidden">Home</h1>
-      <FFModal
-        title="Confirm Deletion"
-        topLeftIcon={<ColouredWarningIcon />}
-        message={`Are you sure you want to delete this review? You won't be able to revoke this decision.`}
-        cancellable={true}
+      <FFRating
+        value={value}
+        handleRatingChange={(e) => setValue(Number(e.target.value))}
       />
-      <FFRating value={rating} handleRatingChange={handleRatingChange} />
     </>
   )
 }
