9225498196a8699f320876126b23c2ee0e486841
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-21 19:15:06 +0530

    FFReviewManageModal added

--- src/components/atoms/FFRating/FFRating.jsx
+++ src/components/atoms/FFRating/FFRating.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Rating, Typography } from '@mui/material'
+import { Rating, Typography, styled } from '@mui/material'
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded'
+import StarRoundedIcon from '@mui/icons-material/StarRounded'
 
-const FFRating = ({ value, handleRatingChange, size }) => {
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#DEB225',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#DEB225',
+  },
+})
+const FFRating = ({ value, handleRatingChange, size, ratingStyles }) => {
   return (
     <>
       <div
@@ -9,10 +19,12 @@ const FFRating = ({ value, handleRatingChange, size }) => {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'flex-start',
+          marginInline: '20px',
+          marginTop: '24px',
+          ...ratingStyles,
         }}
       >
-        <Rating
-          name="half-rating"
+        <StyledRating
           defaultValue={0}
           value={value}
           precision={0.5}
@@ -20,8 +32,18 @@ const FFRating = ({ value, handleRatingChange, size }) => {
           sx={{
             fontSize: size,
           }}
+          icon={<StarRoundedIcon fontSize="inherit" />}
+          emptyIcon={<StarBorderRoundedIcon fontSize="inherit" />}
         />
-        <Typography>{value}</Typography>
+        <Typography
+          sx={{
+            fontSize: '20px',
+            fontWeight: '700',
+            marginLeft: '25px',
+          }}
+        >
+          {value}
+        </Typography>
       </div>
     </>
   )
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import FFRating from '../../components/atoms/FFRating/FFRating'
-
+import FFReviewManageModal from '../../components/molecules/FFModal/FFReviewManageModal'
+import ColouredNotepadIcon from '../../assets/icons/ColouredNotepadIcon'
+import FFConfirmationModal from '../../components/molecules/FFModal/FFConfirmationModal'
+import ColouredWarningIcon from '../../assets/icons/ColouredWarningIcon'
 const Index = () => {
   const [value, setValue] = useState(0)
+  console.log('coloredNotepadIcon', ColouredNotepadIcon)
   return (
     <>
       <h1 className="hidden">Home</h1>
@@ -11,6 +15,12 @@ const Index = () => {
         value={value}
         size={'3rem'}
         handleRatingChange={(e) => setValue(Number(e.target.value))}
+      />
+      <FFReviewManageModal
+        title={'Write a Review'}
+        subTitle={'Share your thoughts about this product'}
+        topLeftIcon={<ColouredNotepadIcon />}
+        firstLabelName={'Review'}
       />
     </>
   )
