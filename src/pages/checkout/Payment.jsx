5186b407ee06255a89a9d64022feb230132198e0
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-05-22 20:11:22 +0530

    Stripe payment issue fixing

--- src/pages/checkout/Payment.jsx
+++ src/pages/checkout/Payment.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 
 import CheckoutForm from './CheckoutForm'
+import { getClientSecret } from '../../services/payment'
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(
   'pk_test_51N3x7aSBDY9rlLLc3us7ndRplJVM9C6kLRq5ep9SX9Kzb3Ug8hI4AQft2crySSvEppVLX4N3llixuml35V6Duvve00DA0OR2Hj',
 )
+const data = {
+  amount: '1000',
+  currency: 'usd',
+}
 
 function Payment() {
+  const [secret, setSecret] = useState('')
+
+  useEffect(() => {
+    getClientSecret(data)
+      .then((res) => setSecret(res))
+      .catch((err) => console.error(err))
+  }, [])
+
   const options = {
     // passing the client secret obtained in step 3
-    clientSecret: '{{CLIENT_SECRET}}',
+    clientSecret: secret,
     // Fully customizable with appearance API.
     appearance: {
       /*...*/
@@ -21,9 +34,11 @@ function Payment() {
   }
 
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements>
+    <>
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm />
+      </Elements>
+    </>
   )
 }
 
--- src/services/payment.js
+++ src/services/payment.js
@@ -1,1 +1,10 @@
-
+export const getClientSecret = async (data) => {
+  console.log('Coming here: ', data)
+  const { client_secret } = await (
+    await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/api/payment/secret?amount=${data.amount}`,
+    )
+  ).json()
+  console.log('client secret: ', client_secret)
+  return client_secret
+}
